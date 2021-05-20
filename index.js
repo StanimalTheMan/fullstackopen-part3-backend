b145f1c255e970eb85713813ff9805a2b68ff044
StanimalTheMan <sjc591@nyu.edu>
2021-05-19 21:25:53 -0400

    3.9

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // app.use(morgan("tiny")); // ex. 3.7
 app.use(
